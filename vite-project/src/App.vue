e5e802a8d61ab4fa9cf7c14bdb5b1f0ec267b512
Sarah Popelier <sarah.popelier@email.com>
2025-12-08 14:31:14 +0100

    initial commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+
+    <a href="#" @click.prevent="onListClick">
+      Voir la liste des données météo
+    </a>
+
+  </div>
+
+</template>
+
+<script setup>
+
+function onListClick() {
+  alert("L'utilisateur a cliqué pour voir la liste");
+}
+
+</script>
+
+<style>
+
+</style>

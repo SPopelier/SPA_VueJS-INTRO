99bf9b1544c0ce7584de15aea12dff5461c67d6e
Sarah Popelier <sarah.popelier@hotmail.fr>
2025-12-09 11:27:22 +0100

    remis au propre, et initialisation des routeurs

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import CitiesList from '../views/CitiesList.vue';
-import Home from '../views/Home.vue'; // si tu as une page d'accueil
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: Home },
-        { path: '/cities', component: CitiesList }
+        { path: '/', component: CitiesList }
     ]
 });
